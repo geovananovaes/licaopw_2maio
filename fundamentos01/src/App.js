7e2aec284b4df324060ba47b59edcf06d15a87bf
geovananovaes <geovanaonovaes@gmail.com>
2021-05-24 17:21:01 +0000

    Aula01-PrimeiroComponente

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import OlaMundo from './components/basico/OlaMundo'
+
+class App extends Component {
+  render () {
+    return(
+      <div>
+        <h1>Aula 01 - Fundamentos 01</h1>
+
+        <OlaMundo />
+
+      </div>
+    )
+  }
+}
+export default App;
